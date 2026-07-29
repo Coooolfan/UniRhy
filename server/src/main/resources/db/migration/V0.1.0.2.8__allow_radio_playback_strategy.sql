e7d1298b9f86c96ebdeec28a855fc7bfc370d0a4
Coolfan <coolfan1024@gmail.com>
2026-07-29 14:39:12 +0800

    refactor(server): 重建数据库迁移基线

--- server/src/main/resources/db/migration/V0.1.0.2.8__allow_radio_playback_strategy.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE public.play_queue
-    DROP CONSTRAINT play_queue_strategy_check;
-
-ALTER TABLE public.play_queue
-    ADD CONSTRAINT play_queue_strategy_check CHECK (
-        playback_strategy IN ('SEQUENTIAL', 'SHUFFLE', 'SINGLE', 'RADIO')
-        );
-
-DROP INDEX IF EXISTS public.uq_async_task_log_artist_normalization_active;
